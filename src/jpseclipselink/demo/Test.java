dc67c0eaffc7cb91b16eeb2de4949860fe452763
YU GU <guyuissc@cn.ibm.com>
2019-03-26 14:33:34 +0800

    save

--- src/jpseclipselink/demo/Test.java
+++ src/jpseclipselink/demo/Test.java
@@ -14,10 +14,29 @@ public static void main(String[] args) {
         EntityTransaction transaction = entityManager.getTransaction();  
         transaction.begin();  
        
-        Person user = new Person();  
-        user.setFirstName("Jerry_SAP");
-        user.setLastName("Wang");
-        entityManager.persist(user);  
+        
+//        Person user = new Person();  
+//        user.setFirstName("Jerry_SAP");
+//        user.setLastName("Wang");
+//        entityManager.persist(user);  
+        
+//        TA ta = new TA();
+//        ta.setId(3);
+//        ta.setName("abcd");
+//        entityManager.persist(ta);
+//        
+//        TB tb = new TB();
+//        tb.setId(2);
+//        tb.setTA(ta);
+//        tb.setName("abcd");
+//        entityManager.persist(tb);
+        
+        TA ta2 = entityManager.find(TA.class, 1);
+        TB tb2 = new TB();
+        tb2.setId(3);
+        tb2.setTA(ta2);
+        tb2.setName("ABCD");
+        entityManager.persist(tb2);
         
         transaction.commit();  
         entityManager.close();  
